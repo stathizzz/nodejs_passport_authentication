c2c687039d77ff7138d2fffbc13a81f078183503
Sfecas Efstathios <stathizzz@gmail.com>
2014-01-21 17:38:25 +0200

    'cont'

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2003-2013, Efstathios D. Sfecas  <stathizzz@gmail.com>
+ Copyright (c) 2013-2014, Efstathios D. Sfecas  <stathizzz@gmail.com>
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
